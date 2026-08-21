00d19edb8d6c4a7f7b89780e0011855b0e487fd9
Hanbie Ryu <hanbie.ryu@gmail.com>
2026-08-21 20:29:45 +0900

    feat: update drone lab build

--- DroneLab/Build/Builds.loader.js
+++ DroneLab/Build/Builds.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 55930695,
+    wasmFileSize: 57797695,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
